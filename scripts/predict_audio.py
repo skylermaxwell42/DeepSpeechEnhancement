0aa95ebce50e4c021289031beba21a258bfbd586
Skyler Maxwell <maxwels2@tcnj.edu>
2018-12-07 14:51:09 -0500

    WIP

--- scripts/predict_audio.py
+++ scripts/predict_audio.py
@@ -1,13 +1,12 @@
 import os
 import argparse
-import progressbar
 import numpy as np
-import matplotlib.pyplot as plt
 from keras.models import load_model
+import matplotlib.pyplot as plt
 
-from AudioProcessing.DataUtils import load_wav_files
+from AudioProcessing.DataUtils import load_wav_files, generate_specgram, AudioSample
 from AudiProcessing.TrainUtils import enhance_speech
-
+from AudioProcessing.SpectrogramUtils import invert_pretty_spectrogram
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Script to train a Speech Enhancement Neural Network')
@@ -45,7 +44,31 @@ if __name__ == '__main__':
     autoencoder = load_model(args.model_dir)
 
     for i, audio_sample in enumerate(input_samples):
+        spcgram = np.transpose(generate_specgram(audio_sample))
+
+        reconstructed_specgram = autoencoder.predict(np.reshape(1, 256, 496, 1))
+
+        reconstructed_sample = invert_pretty_spectrogram(reconstructed_specgram)
+
+        AudioSample(reconstructed_sample).write_wav_file(args.output_dir+'/enhanced_wav_{}.wav'.format(i))
+
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 4))
+        cax = ax.matshow(spcgram, interpolation='nearest', aspect='auto', cmap=plt.cm.afmhot,
+                         origin='lower')
+        fig.colorbar(cax)
+        plt.title('Target Spctrogram')
+        fig.savefig(os.path.join(args.output_dir, 'clean_spectrogram_{}.png'.format(i)))
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 4))
+        cax = ax.matshow(reconstructed_specgram, interpolation='nearest', aspect='auto', cmap=plt.cm.afmhot,
+                         origin='lower')
+        fig.colorbar(cax)
+        plt.title('Noisy Spectrogram')
+        fig.savefig(os.path.join(args.output_dir, 'noise_spectrogram_{}.png'.format(i)))
+        i = i + 1
+
+    '''
+    for i, audio_sample in enumerate(input_samples):
         reconstructed = enhance_speech(autoencoder, audio_sample)
         audio_sample.write_wavfile(os.path.join(args.output_dir, 'input_sample_{}.wav'.format(i)))
         reconstructed.write_wavfile(os.path.join(args.output_dir, 'reconstructed_{}.wav'.format(i)))
-
+    '''
